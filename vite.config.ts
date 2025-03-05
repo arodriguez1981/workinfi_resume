ac68aac3b6ce50db7e05b5dd0728692130f0e74e
arodriguez1981 <arodriguez1981@gmail.com>
2025-03-05 12:04:07 -0500

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
 export default defineConfig({
-  base: "/workinfi_resume/"
+  base: "/workinfi_resume/",
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react']
